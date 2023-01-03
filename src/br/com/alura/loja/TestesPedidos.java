bc95f07bf749314fab1377cce5ce6b822dacd586
Jean Cruz <jeansc@outlook.com>
2023-01-03 20:32:44 -0300

    Command

---
+++ src/br/com/alura/loja/TestesPedidos.java
@@ -0,0 +1,19 @@
+package br.com.alura.loja;
+
+import br.com.alura.loja.orcamento.Orcamento;
+import br.com.alura.loja.pedido.GeraPedido;
+import br.com.alura.loja.pedido.Pedido;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class TestesPedidos {
+    public static void main(String[] args) {
+        String cliente = args[0];
+        BigDecimal valorOcamento = new BigDecimal(args[1]);
+        int quantidadeItens = Integer.parseInt(args[2]);
+
+        GeraPedido gerador = new GeraPedido(cliente,valorOcamento,quantidadeItens);
+        gerador.executa();
+    }
+}
