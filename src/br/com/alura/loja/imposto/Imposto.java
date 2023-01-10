8164c044b814769a758efe83d2e1d5b82e724e84
Jean Cruz <jeansc@outlook.com>
2023-01-10 20:47:25 -0300

    Aplicando o pattern Decorator

--- src/br/com/alura/loja/imposto/Imposto.java
+++ src/br/com/alura/loja/imposto/Imposto.java
@@ -4,6 +4,22 @@ import br.com.alura.loja.orcamento.Orcamento;
 
 import java.math.BigDecimal;
 
-public interface Imposto {
-   BigDecimal calcular(Orcamento orcamento);
+public abstract class Imposto {
+
+   private Imposto outro;
+
+   public Imposto(Imposto outro) {
+      this.outro = outro;
+   }
+   protected abstract BigDecimal realizarCaluclo(Orcamento orcamento);
+
+   public BigDecimal calcular(Orcamento orcamento){
+      BigDecimal valorImposto = realizarCaluclo(orcamento);
+      BigDecimal valorDoOutroImposto = BigDecimal.ZERO;
+      if (outro != null){
+         valorDoOutroImposto = outro.realizarCaluclo(orcamento);
+      }
+      return valorImposto.add(valorDoOutroImposto);
+
+   }
 }
